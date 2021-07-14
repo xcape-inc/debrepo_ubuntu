9516ae90a47d5ae0b350304b3dc759acfae0059e
Gene Erik <geneerik@fossdevops.com>
2021-07-14 02:45:16 -0700

    Code to make the debian repo generate

---
+++ add_deb_to_repo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+trap 'catch $? $LINENO' ERR
+catch() {
+  echo "Error $1 occurred on $2"
+}
+PARAMS=$1
+if [[ "${PARAMS}" == '' ]]; then
+  echo 'Script requires parameters'; false
+fi
+set -euo pipefail
+PARAMS=("$@")
+TARGET_FILE_PATH="${PARAMS[0]}"
+DISTRO_RELEASE_CODENAME="${DISTRO_RELEASE_CODENAME:-focal}"
+
+(dpkg -I "${TARGET_FILE_PATH}" | grep '^ Section:') && PACKAGE_HAS_SECTION=1 || PACKAGE_HAS_SECTION=0
+
+# allows the package to default to utils if no section defined in deb
+if [[ "${TARGET_FILE_PATH}" == "1" ]]; then
+  reprepro --outdir ./REPOSITORY.PATH includedeb "${DISTRO_RELEASE_CODENAME}" "${TARGET_FILE_PATH}"
+else
+  reprepro -S utils --outdir ./REPOSITORY.PATH includedeb "${DISTRO_RELEASE_CODENAME}" "${TARGET_FILE_PATH}"
+fi
